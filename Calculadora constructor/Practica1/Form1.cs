4519f5a4e27b47d31830b293fc9c2349613a1836
Yamzx14 <99759853+Yamzx14@users.noreply.github.com>
2022-09-19 22:38:30 -0500

    Add files via upload

---
+++ Calculadora constructor/Practica1/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            elsuma osuma = new elsuma
+                 (int.Parse(textBox1.Text),
+                 int.Parse(textBox2.Text));
+           /* elsuma osuma = new elsuma();
+            osuma.N1 = int.Parse(textBox1.Text);
+            osuma.N2 = int.Parse(textBox2.Text);*/
+
+            textBox3.Text = osuma.sumar().ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+    
+            elsuma osuma = new elsuma
+                (int.Parse(textBox1.Text),
+                int.Parse(textBox2.Text));
+            textBox3.Text = osuma.meno().ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+         
+            elsuma osuma = new elsuma
+                (int.Parse(textBox1.Text),
+                int.Parse(textBox2.Text));
+            textBox3.Text = osuma.dividir().ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            elsuma osuma = new elsuma
+                (int.Parse(textBox1.Text),
+                int.Parse(textBox2.Text));
+            textBox3.Text = osuma.multi().ToString();
+        }
+    }
+}
---
+++ Calculadora constructor/Practica1/elsuma.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1
+{
+    class elsuma
+    {
+        //campo propiedades, como es
+        private int n1;
+        private int n2;
+
+       
+          public elsuma(int n1, int n2)
+          {
+              this.n1 = n1;
+              this.n2 = n2;
+          }
+
+         
+          public int sumar()
+          {
+              return (n1 + n2);
+          }
+
+          public double meno()
+          {
+              return (n1 - n2);
+          }
+          public double dividir()
+          {
+              return (n1 / n2);
+          }
+          public double multi()
+          {
+              return (n1 * n2);
+          }
+    }
+}
